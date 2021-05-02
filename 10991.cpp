b1dc9087b7c78db66d6728b25428c2c9009e2578
Hong <fheldgktpdy@naver.com>
2021-05-02 17:49:07 +0900

    0502

---
+++ 10991.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+int main() {
+	int k;
+	std::cin >> k;
+	for (int i = 1; i <= k; i++) {
+		for (int s = i; s < k; s++)
+			std::cout << " ";
+		for (int z = 1; z <= i; z++)
+			std::cout << "* ";
+		std::cout << "\n";
+	}
+}
