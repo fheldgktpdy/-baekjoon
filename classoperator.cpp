e42a8bdea220e686932ba5963c62c072b78366c8
Hong <fheldgktpdy@naver.com>
2021-05-31 12:03:39 +0900

    dot and arrow

---
+++ classoperator.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include<cmath>
+#include<fstream>
+
+class Assembly {
+public:
+	int value;
+};
+
+int main() {
+	Assembly* ptr_a = new Assembly;
+	ptr_a->value = 5;
+	(*ptr_a).value = 5;
+
+
+
+}
