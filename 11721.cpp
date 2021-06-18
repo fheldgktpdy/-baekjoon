a0695a6b192b3e08d0b5e5cdb5bc8ab49ad3b8e1
Hong <fheldgktpdy@khu.ac.kr>
2021-06-18 13:24:41 +0900

    substr_func

---
+++ 11721.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+int main() {
+	string khu;
+	cin >> khu;
+	int len = khu.length();
+	int k = len / 10;
+	for (int i = 0; i < k; i++)
+		cout << khu.substr(i*10, 10)<<'\n';
+	cout << khu.substr(k * 10);
+
+}
