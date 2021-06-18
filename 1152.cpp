fe326a81e681cc5fbd2e58a5cc8d02856733f735
Hong <fheldgktpdy@khu.ac.kr>
2021-06-18 13:24:20 +0900

    getline_func

---
+++ 1152.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+
+using namespace std;
+int main() {
+	string khu;
+	getline(cin, khu);
+	int count = 0;
+	for (int i = 0; i < khu.length(); i++) {
+		if (khu[i] == ' ')
+			count++;
+	}
+	if (khu[0] == ' ')
+		count--;
+	if (khu.back() == ' ')
+		count--;
+	cout << ++count;
+}
