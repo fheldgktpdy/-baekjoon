4773487b251cd43e69c26a36afb71981fced2781
Hong <fheldgktpdy@khu.ac.kr>
2021-06-22 12:49:36 +0900

    bronze1

---
+++ 2851.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+int main() {
+	vector<int> vec;
+	int sum=0;
+	for (int i = 0; i < 10; i++) {
+		int t;
+		cin >> t;
+		vec.push_back(t);
+	}
+	for (int k = 0; k < vec.size(); k++) {
+		sum += vec[k];
+		if (sum == 100) {
+			cout << sum;
+			break;
+		}
+		else if (sum > 100) {
+			if (sum - vec[k] < 100) {
+				if (100 - (sum - vec[k]) < sum - 100) {
+					cout << sum - vec[k];
+					break;
+				}
+				else {
+					cout << sum;
+					break;
+				}
+			}
+		}
+	}
+	if (sum < 100)
+		cout << sum;
+}
