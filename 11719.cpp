0cb892c1c0804838346755005c5146fba09abfdf
Hong <fheldgktpdy@khu.ac.kr>
2021-06-22 20:03:47 +0900

    bronze1

---
+++ 11719.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cstring>
+#include<string>
+#include <vector>
+
+using namespace std;
+int main()
+{
+	string input;
+
+	for (int i = 0; i < 100; i++)
+	{
+		getline(cin, input);  
+		cout << input << endl;
+	}
+
+	return 0;
+}
