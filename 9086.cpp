ea36a3e220d74491c3ee542bd5e99589139f0136
Hong <fheldgktpdy@khu.ac.kr>
2021-06-22 12:25:18 +0900

    bronze2

---
+++ 9086.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		string a;
+		cin >> a;
+		if (a.length() == 1)
+			cout << a[0] << a[0] << '\n';
+		else
+			cout << a[0]<<a[a.length() - 1]<<'\n';
+	}
+}
