ed233dfa8ed4bb04533065dcb69b9275a9728795
Hong <fheldgktpdy@khu.ac.kr>
2021-06-23 12:17:16 +0900

    bronze1

---
+++ 10769.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cstring>
+#include<string>
+#include <vector>
+#include<queue>
+
+using namespace std;
+int main()
+{
+	string t;
+	getline(cin, t);
+	int sum1 = 0;
+	int sum2 = 0;
+	for (int i = 0; i < t.length(); i++) {
+		if (t[i] == ':' && t[i + 1] == '-' && t[i + 2] == ')')
+			sum1 += 1;
+		else if (t[i] == ':' && t[i + 1] == '-' && t[i + 2] == '(')
+			sum2 += 1;
+	}
+	if (sum1 > sum2)
+		cout << "happy";
+	if (sum1 == sum2) {
+		if (sum1 == 0 && sum2 == 0)
+			cout << "none";
+		else
+			cout << "unsure";
+	}
+	if (sum1 < sum2)
+		cout << "sad";
+
+}
