a61b64694314debc07b43ac44dfaadd42bc56f9c
Hong <fheldgktpdy@khu.ac.kr>
2021-06-24 12:09:17 +0900

    silver5

---
+++ 1292.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cstring>
+#include<string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+	int a, b;
+	cin >> a >> b;
+	vector<int>vec;
+	int t = 1;
+	while (true) {
+		
+		for (int i = 0; i < t; i++) {
+			vec.push_back(t);
+		}
+		t++;
+		if (t == 45) {
+			for (int k = 0; k < 10; k++) {
+				vec.push_back(45);
+			}
+			break;
+		}
+	}
+	int sum = 0;
+	for (int q = a-1; q <= b-1; q++) {
+		sum += vec[q];
+	}
+	cout << sum;
+}
