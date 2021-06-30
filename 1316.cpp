a5be600338a6f85c5f956ca9d8b7f6ebf219643f
Hong <fheldgktpdy@khu.ac.kr>
2021-06-30 13:40:11 +0900

    silver5

---
+++ 1316.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include<cstring>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int sum = n;
+	
+	for(int i = 0; i < n; i++) {
+		bool cham = true;
+		string a;
+		cin >> a;
+		if (a.length() == 1)
+			continue;
+		else {
+			for (int k = 0; k < a.length() - 1; k++) {
+				for (int j = k + 1; j < a.length(); j++) {
+					if (a[k] == a[j] && a[j - 1] != a[j]) {
+						sum -= 1;
+						cham = false;
+						break;
+					}
+				}
+				if (cham == false)
+					break;
+			}
+		}
+	}
+	cout << sum << '\n';
+}
