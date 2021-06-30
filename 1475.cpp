01375ebeb56fc3d62ba3f6a58f4087098c38bfa5
Hong <fheldgktpdy@khu.ac.kr>
2021-06-30 16:49:08 +0900

    silver5

---
+++ 1475.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+#include<cstring>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int arr[10] = { 0 };
+	int gori;
+	int max = 0;
+
+	if (n == 0)
+		cout << 1;
+	else {
+		while (n > 0) {
+			gori = n % 10;
+			arr[gori] += 1;
+			n = n / 10;
+		}
+
+		int sixn = arr[6];
+		int ninen = arr[9];
+		int k = (sixn + ninen);
+		if (k % 2 != 0)
+			k += 1;
+
+		for (int i = 0; i < 10; i++) {
+			if (i == 6 || i == 9)
+				continue;
+			else {
+				if (max <= arr[i])
+					max = arr[i];
+			}
+		}
+
+		if (max > k / 2)
+			cout << max;
+		else
+			cout << k / 2;
+	}
+}
