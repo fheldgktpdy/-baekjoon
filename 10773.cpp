ee22b89f7cf57e3ae3290b25d99b2dff694855c5
Hong <fheldgktpdy@khu.ac.kr>
2021-07-01 14:12:07 +0900

    silver4

---
+++ 10773.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include<cstring>
+
+using namespace std;
+
+int main() {
+	int k;
+	cin >> k;
+	vector<int>vec;
+	int sum = 0;
+	for (int i = 0; i < k; i++) {
+		int n;
+		cin >> n;
+		if (n == 0)
+			vec.pop_back();
+		else
+			vec.push_back(n);
+	}
+	for (int g = 0; g < vec.size(); g++) {
+		sum += vec[g];
+	}
+	cout << sum;
+}
