879f2a849d9cb76243a4c944271dedf3cafb4dbf
Hong <fheldgktpdy@khu.ac.kr>
2021-07-01 12:38:20 +0900

    silver5

---
+++ 14467.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+#include<cstring>
+
+using namespace std;
+
+int main() {
+	int n;
+	int cownum;
+	int cowpos;
+	int sum=0;
+	vector<int>vec(11,9);
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> cownum >> cowpos;
+		if (vec[cownum] == 9) {
+			vec[cownum] = cowpos;
+		}
+		else {
+			if (vec[cownum] == cowpos)
+				continue;
+			else {
+				sum += 1;
+				vec[cownum] = cowpos;
+			}
+		}
+	}
+	cout << sum;
+}
