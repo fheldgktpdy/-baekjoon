374f118e3fb21a34aabdd2a67ef70da0b432f428
Hong <fheldgktpdy@khu.ac.kr>
2021-07-08 22:19:13 +0900

    leetcode

---
+++ leetcode/58.cpp
@@ -0,0 +1,29 @@
+﻿class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int sum = 0;
+        int k = 0;//문자열의 시작
+        if (s[s.length() - 1] == ' ') {
+            for (int i = s.length() - 1; i >= 0; i--)
+                if (s[i] != ' ') {
+                    k = i;
+                    break;
+                }
+            for (int j = k; j >= 0; j--) {
+                if (s[j] != ' ')
+                    sum += 1;
+                else
+                    break;
+            }
+        }
+        else {
+            for (int i = s.length() - 1; i >= 0; i--) {
+                if (s[i] != ' ')
+                    sum += 1;
+                else
+                    break;
+            }
+        }
+        return sum;
+    }
+};
